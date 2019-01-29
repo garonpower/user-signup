fe7856af1348ec4d0ed00ae1f63139f7de2dc2bb
Garon Power <garonpower@yahoo.com>
2019-01-29 19:35:23 +0000

    initialized project

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template('home_page.html', title="signup")
+
+app.run()
